4f83622c19809357b58ff07ce4561aca5da841c8
lzx <lzx@163.com>
2022-11-04 20:04:36 +0800

    first commit

---
+++ src/main/java/Test.java
@@ -0,0 +1,25 @@
+/**
+ * @Package PACKAGE_NAME
+ * @author 刘志轩
+ * @date 2022/11/4 20:00
+ * @version V1.0
+ * @Copyright © 2021-2022 有限公司
+ */
+
+/**
+ *
+ * @title: Test
+ * @Author liuzhixuan
+ * @Date: 2022/11/4 20:00
+ * @Version 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("git idea test");
+        System.out.println("Hello atguigu");
+        System.out.println("Hello atguigu");
+        System.out.println("Hello atguigu");
+        System.out.println("Hello atguigu");
+        System.out.println("Hello atguigu");
+    }
+}
